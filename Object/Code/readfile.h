2e39ec8705cd370a40febc57a907b93ae32a8fca
seoyounji <wltjdus0208@naver.com>
2019-10-30 20:01:36 +0900

    Delete readfile.h

--- Object/Code/readfile.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-#include <fstream>
-#include <string>
-using namespace std;
-
-bool ReadFile(const char* pFileName, string& outFile);
-string ReadFile(const char* pFileName);
