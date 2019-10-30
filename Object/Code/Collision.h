7642b568c70761f0d09c0ee668db8da439ff9205
seoyounji <wltjdus0208@naver.com>
2019-10-30 19:56:28 +0900

    Delete Collision.h

--- Object/Code/Collision.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <glm/vec3.hpp>
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
-bool Collision(glm::vec3 position1, float boundary1[6],  glm::vec3 position2, float boundary2[6]);
-bool MapCollisionx(float mapboundary[6], glm::vec3 position, float boundary[6], int dir);
-bool MapCollisionz(float mapboundary[6], glm::vec3 position, float boundary[6], int dir);
-glm::vec3 Reposition(glm::vec3 activeP, float activeboundary[6], glm::vec3 staticP, float staticboundary[6]);
